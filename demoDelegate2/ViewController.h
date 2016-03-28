fc87bda6dda7814674cfd46c00b425ef0feb9272
姚健 <YaoJian@yjdemacbook-air.local>
2016-03-28 15:26:09 +0800

    Initial Commit

---
+++ demoDelegate2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  demoDelegate2
+//
+//  Created by YJ on 16/3/28.
+//  Copyright © 2016年 YJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
